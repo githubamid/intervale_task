3c407abe9a31fccde9c4e6b68717d2c4ccc687f8
Dmitry Khomich <hodv@tut.by>
2020-03-11 16:09:15 +0300

    ESXi, Oracle, Wildfly

--- ansible/roles/applic/files/launch.sh
+++ ansible/roles/applic/files/launch.sh
@@ -7,5 +7,5 @@ fi
 if [[ "$1" == "domain" ]]; then
     $WILDFLY_HOME/bin/domain.sh -c $2 -b $3
 else
-    $WILDFLY_HOME/bin/standalone.sh -c $2 -b $3 -bmanagement=0.0.0.0
+    $WILDFLY_HOME/bin/standalone.sh -c $2 -b $3
 fi
